d08c16b7aa0641fc53019fd5726b53af75f1836c
abdulsamadexe <abdulsamadbrohi461@gmail.com>
2024-12-21 17:39:13 +0500

    Refactor SchoolManagementSystem: remove commented-out code and streamline grade assignment process

--- SchoolManagementSystem.java
+++ SchoolManagementSystem.java
@@ -5,54 +5,7 @@ public class SchoolManagementSystem {
         University university = new University();
         university.loadData("data.txt");
 
-<<<<<<< HEAD
         university.assignGradeToStudent("S1001", "C1001", 60);
-=======
-        // Student student1 = new Student("S1001", "Alice", "abc@gmail.com", "2000-01-01", "123 Main St");
-        // Student student2 = new Student("S1002", "Bob", "dsa", "2000-01-02", "456 Main St");
-        // Student student3 = new Student("S1003", "Charlie", "sdf", "2000-01-03", "789 Main St");
-
-        // Teacher teacher1 = new Teacher("T1001", "David", "@gmailc.com", "1980-01-01", "123 Main St");
-        // Teacher teacher2 = new Teacher("T1002", "Eve", "sdf", "1980-01-02", "456 Main St");
-
-        Course course1 = new Course("C1221", "Math", 3);
-        // String title ="Data Science";
-        // title = title.replace(" ", "_");
-        // Course course4 = new Course("C1464", title, 4);
-        university.addCourse(course1);
-        
-        // university.assignTeacherToCourse("T021", "C054");
-        university.enrollStudentInCourse("S1003", "C1221");
-        // university.assignGradeToStudent("S9999", "C1221", 45);
-        // university.addStudent(student3);
-        // university.addStudent(student2);
-        // university.addStudent(student1);
-
-        // university.addTeacher(teacher2);
-        // university.addTeacher(teacher1);
-        
-        // university.addCourse(course2);
-        // university.addCourse(course1);
-
-
-        // teacher1.assignCourse(course1);
-        // teacher2.assignCourse(course2);
-
-        // course1.addStudent(student1);
-        // course1.addStudent(student2);
-
-        // course2.addStudent(student1);
-        // course2.assignGrade(student1, 45);
-
-        // AdministrativeStaff staff1 = new AdministrativeStaff("A1001", "Frank", "abcder@gmail.com", "1970-01-01", "123 Main St", "Manager");
-
-        // AdministrativeStaff staff2 = new AdministrativeStaff("A1002", "Grace", "456@gmail.com", "1970-01-02", "456 Main St", "Supervisor");
-
-        // university.addAdminstaff(staff2);
-        // university.addAdminstaff(staff1);
-
-        // System.out.println(university.getCourses().get(1).calculateAverageGrade());
->>>>>>> ba007fa1ed37b5d52f6eaa6903c88e635179118c
         
 
         university.saveData("data.txt");
